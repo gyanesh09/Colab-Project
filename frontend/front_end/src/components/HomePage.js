63e05ceb89e3818024a5c32715c6f024d6cd8799
Praneet <praneetkaurbhatia@gmail.com>
2021-10-18 19:25:40 +0530

    Designed Basic Layout

---
+++ frontend/front_end/src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar';
+import Carousel from './Carousel';
+import Cards from './Cards.js'
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavBar />
+            <Carousel />
+
+            <Cards />
+        </div>
+    )
+}
+
+export default HomePage
