e7a4e1f1298a8253fa9bc3fd0c3b5a45d909faac
Praneet <praneetkaurbhatia@gmail.com>
2021-11-06 13:06:30 +0530

    Created Basic Structure

---
+++ frontend/AngularFrontend/src/app/components/components.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ExploreComponent } from './explore/explore.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CardComponent } from './card/card.component';
+import { CardsComponent } from './cards/cards.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavBarComponent,
+    CarouselComponent,
+    ExploreComponent,
+    AboutComponent,
+    ContactUsComponent,
+    CardComponent,
+    CardsComponent,
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomeComponent,
+    NavBarComponent,
+    CarouselComponent,
+    ExploreComponent ,
+     AboutComponent ,
+    ContactUsComponent ,
+     CardComponent ,
+     CardsComponent ,
+     RegisterComponent, 
+    LoginComponent 
+  ]
+})
+export class ComponentsModule { }
